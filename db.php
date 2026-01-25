4f2f8855df27d666a7e6f445375f516e4fa4fc97
rapeephatz <129982751+rapeephatz@users.noreply.github.com>
2026-01-26 01:12:23 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,12 @@
 <?php
-$conn = new mysqli("localhost","root","","attendance");
+$host = getenv("MYSQL_HOST") ?: "localhost";
+$user = getenv("MYSQL_USER") ?: "root";
+$pass = getenv("MYSQL_PASSWORD") ?: "";
+$db   = getenv("MYSQL_DATABASE") ?: "attendance";
+$port = getenv("MYSQL_PORT") ?: 3306;
+
+$conn = new mysqli($host,$user,$pass,$db,$port);
+
 if ($conn->connect_error) {
-    die("DB Error");
+    die("DB Error: " . $conn->connect_error);
 }
