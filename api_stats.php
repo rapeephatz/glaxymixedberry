eaf73076709051acc12e60612bae5c025ab9ae2c
rapeephatz <129982751+rapeephatz@users.noreply.github.com>
2026-01-26 13:17:23 +0700

    Update api_stats.php

--- api_stats.php
+++ api_stats.php
@@ -1,25 +1,20 @@
 <?php
 include "db.php";
 
-$today = date("Y-m-d");
+$users = $conn->query("SELECT COUNT(*) as total FROM users")
+              ->fetch_assoc()['total'];
 
-/* จำนวน user */
-$u = $conn->query("SELECT COUNT(*) as total FROM users")
-          ->fetch_assoc()['total'];
-
-/* วันนี้ */
-$t = $conn->query("
+$today = $conn->query("
 SELECT COUNT(*) as total 
 FROM checkins 
-WHERE DATE(created_at) = '$today'
+WHERE DATE(time) = CURDATE()
 ")->fetch_assoc()['total'];
 
-/* ทั้งหมด */
-$c = $conn->query("SELECT COUNT(*) as total FROM checkins")
-          ->fetch_assoc()['total'];
+$total = $conn->query("SELECT COUNT(*) as total FROM checkins")
+              ->fetch_assoc()['total'];
 
 echo json_encode([
-    "users" => $u,
-    "today" => $t,
-    "total" => $c
+    "users" => $users,
+    "today" => $today,
+    "total" => $total
 ]);
