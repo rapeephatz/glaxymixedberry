ea6b1ea2091efc365f67febe29dd2161fece1447
VIPER <thehart2008@gmail.com>
2026-01-28 15:12:07 +0700

    check error past2

--- checkin.php
+++ checkin.php
@@ -2,11 +2,6 @@
 session_start();
 include "db.php";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 /* ==============================
    เช็ค session
    ============================== */
--- dashboard.php
+++ dashboard.php
@@ -2,6 +2,11 @@
 session_start();
 include "db.php";
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 if(!isset($_SESSION['user'])){
     header("Location: index.php");
     exit();
