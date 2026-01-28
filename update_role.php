8f3fb43088e6510c0b29b7a11d00115bb56a3ad2
VIPER <thehart2008@gmail.com>
2026-01-28 17:52:11 +0700

    update small

--- update_role.php
+++ update_role.php
@@ -8,6 +8,11 @@ if($_SESSION['user']['role'] != 'admin'){
 
 $id = $_POST['id'];
 $role = $_POST['role'];
+$allowed = ['user','staff','admin'];
+if(!in_array($role, $allowed)){
+    exit('Invalid role');
+}
+
 
 $conn->query("UPDATE users SET role='$role' WHERE discord_id='$id'");
 header("Location: users.php");
